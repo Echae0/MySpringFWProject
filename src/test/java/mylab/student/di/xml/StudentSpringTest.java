feb96e7e0df23ea99012e5efaf377b931700a8db
Echae0 <edujcm01@gmail.com>
2025-04-25 15:35:53 +0900

    course test update

--- src/test/java/mylab/student/di/xml/StudentSpringTest.java
+++ src/test/java/mylab/student/di/xml/StudentSpringTest.java
@@ -2,6 +2,9 @@ package mylab.student.di.xml;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +18,35 @@ public class StudentSpringTest {
 	@Autowired
 	Course course;
 	
+	@Resource(name = "gradeService")
+	GradeService service;
+	
 	@Test
+	void testService() {
+		assertNotNull(service);
+		assertEquals("C001", service.getCourse().getCourseId());
+		assertEquals("A", service.calculateGrade("S001"));
+		assertEquals(2, service.getHighScoreStudents(80).size());
+		
+		for(Student student : service.getHighScoreStudents(80)) {
+			System.out.println(student);
+		}
+	}
+	
+		
+	@Test @Disabled
 	void testCourse() {
 		assertNotNull(course);
 		assertEquals("Java Programming", course.getCourseName());
 		assertEquals(3, course.getStudents().size());
-		assertEquals("emma", course.getStudents().get(0).getName());
+		/*
+		 * course.getStudents() => List<Student>  학생목록
+		 * course.getStudents().get(0) => Student 학생
+		 * course.getStudents().get(0).getName() => String 학생의 이름
+		 */
+		assertEquals("엠마", course.getStudents().get(0).getName());
 		
 		System.out.println(course.getAverageScore());
-		
+		assertEquals(83.333, course.getAverageScore(),0.001);
 	}
 }
