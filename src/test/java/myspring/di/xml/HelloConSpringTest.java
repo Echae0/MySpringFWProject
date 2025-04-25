4f504a624ac875bcdd77008372ca7c5dbd7b5693
Echae0 <edujcm01@gmail.com>
2025-04-25 13:42:28 +0900

    commit

--- src/test/java/myspring/di/xml/HelloConSpringTest.java
+++ src/test/java/myspring/di/xml/HelloConSpringTest.java
@@ -23,6 +23,15 @@ public class HelloConSpringTest {
 		hello.print();
 		
 		assertEquals(3, hello.getNames().size());
+		assertEquals("스프링클라우드", hello.getNames().get(2));
+		
+		//hello.getNames() ==> List<String>
+		for(String name: hello.getNames()) {
+			System.out.println(name);
+		}
+		//Iterable인터페이스의 forEach(Consumer) 메서드 호출하기
+		//람다식 함수형 인터페이스
+		
 	}
 	
 }
