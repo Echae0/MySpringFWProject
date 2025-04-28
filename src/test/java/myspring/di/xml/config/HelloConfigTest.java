457395240c796e17d4c6be56e5aabfff600a88a2
Echae0 <edujcm01@gmail.com>
2025-04-28 14:11:45 +0900

    HelloConfigTest update

--- src/test/java/myspring/di/xml/config/HelloConfigTest.java
+++ src/test/java/myspring/di/xml/config/HelloConfigTest.java
@@ -1,11 +1,24 @@
 package myspring.di.xml.config;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = HelloConfig.class)
+@ContextConfiguration(classes = HelloConfig.class )
 public class HelloConfigTest {
+	@Autowired
+	List<String> namesList;
+	
+	@Test
+	void helloConfig() {
+		for (String name : namesList) {
+			System.out.println(name);
+		}
+	}
 	
 }
